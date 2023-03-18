e507a5d3df375508a9786755f93d6cdc83e844cd
Parker Satterfield <parkers205@gmail.com>
2023-03-18 16:18:37 -0500

    feature to add user setting for default editor

--- bin/index.js
+++ bin/index.js
@@ -1,29 +1,22 @@
 #!/usr/bin/env node
 
 import inquirer from "inquirer";
-import fs, { opendir } from "fs";
+import fs from "fs";
 import { exec } from "child_process";
 import { chdir } from "process";
 import clipboard from "clipboardy";
 import chalk from "chalk";
 import yargs from "yargs";
-import { isNotJunk } from 'junk';
-
-// yargs config
-yargs((process.argv.slice(2)))
-  .usage('nav-cli is a command line tool to help developers navigate file systems from the command line better.'
-    + '\n \n Use arrow keys to navigate directories or open files in VS Code'
-    + '\n \n For usage, check out the ReadMe here:' 
-    + '\n https://github.com/parkersatterfield/nav-cli#nav-cli--')
-  .command('nav', "Run to bring up navigation interface in your terminal.")
-  .help('help')
-  .alias('h', 'help')
-  .argv
-;
+import { fileURLToPath } from 'url';
+import path from 'path';
+  
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf-8'));
 
 const STAY_MESSAGE = "✅ Stay Here";
 const GO_BACK_MESSAGE = "🔙 Go Back";
-const OPEN_MESSAGE = "↗️ Open in VS Code"
+const OPEN_MESSAGE = `↗️ Open in ${config.editor}`
 
 // TODO: make this function cd in current shell instance
 // temp solution is copy command to clipboard
@@ -65,9 +58,9 @@ const handleAnswer = async (answer) => {
 
 // open file helper
 const openFile = (file) => {
-    // try open with VS Code
+    // try open with editor
     try {
-        exec(`${file} code .`);
+        exec(`${file} ${config.editorOptions[config.editor].openCommand}`);
     } catch (error) {
         console.error(error);
     }
@@ -76,7 +69,7 @@ const openFile = (file) => {
 // open dir helper
 const openDir = () => {
     try {
-        exec(`code ${process.cwd()}`);
+        exec(`${config.editorOptions[config.editor].openCommand} ${process.cwd()}`);
     } catch (error) {
         console.error(error);
     }
@@ -114,5 +107,51 @@ const nav = async () => {
     });
 }
 
-// initialize program 
-nav();
+// change editor function
+const changeEditor = () => {
+    inquirer.prompt({
+        name: 'editor',
+        type: 'list',
+        message:"Select your default editor",
+        choices: [
+            "VS Code",
+            "IntelliJ"
+        ]
+    }).then((answer) => 
+        writeEditorChange(answer))
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+// change config.json file
+const writeEditorChange = (answer) => {
+    config.editor = answer.editor;
+    fs.writeFileSync(path.join(__dirname, 'config.json'), JSON.stringify(config));
+}
+
+// yargs config
+yargs((process.argv.slice(2)))
+  .usage('nav-cli is a command line tool to help developers navigate file systems from the command line better.'
+  + '\n \n Use arrow keys to navigate directories or open files in editor'
+    + '\n \n For usage, check out the ReadMe here:' 
+    + '\n https://github.com/parkersatterfield/nav-cli#nav-cli--')
+  .command(
+    'nav',
+    "Run to bring up navigation interface in your terminal."
+  )
+  .command(
+    'nav config',
+    "Change default editor"
+  )
+  .help('help')
+  .wrap(null)
+  .alias('h', 'help')
+  .argv
+;
+
+if (process.argv.slice(2) == 'config') {
+    changeEditor();
+} else {
+    nav();
+}
